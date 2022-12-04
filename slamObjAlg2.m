4c581932cedef9840e8872073431605b101b7029
RexCai <crh0126@gmail.com>
2022-12-04 12:44:28 -0800

    Move New Algoritm to Alg2

---
+++ slamObjAlg2.m
@@ -0,0 +1,69 @@
+classdef slamObjAlg2
+    properties (Access = private)
+        m_isFirst % if the first point passed in
+        m_MapCenter % Size of the map
+        m_MapSize % Size of the map (Raise the map size if out of index)
+        m_maxRange % max detecrange of device
+        m_maxSearch; % maxSearch Distance
+        m_resolution % Resolution of each scan
+        m_prevScan % Previous Scan
+        m_currScan % Current Scan
+        m_Map % Map of all cuurent point
+        m_currPos; % localization result
+    end
+    % properties
+    % end
+    methods
+        function obj = slamObjAlg2(maxRange, resolution, searchRange, mapSize) % Class Constructor
+            obj.m_isFirst = false;
+            obj.m_Map = zeros(mapSize);
+            obj.m_MapSize = mapSize;
+            obj.m_MapCenter = [mapSize/2, mapSize/2];
+            obj.m_maxRange = maxRange;
+            obj.m_maxSearch = searchRange;
+            obj.m_resolution = resolution;
+            obj.m_prevScan = zeros(resolution);
+            obj.m_currScan = zeros(resolution);
+            obj.m_currPos = zeros(1,2);
+        end
+
+        function obj = addNode(obj, range, angle) % Add New Lidar result
+            obj.m_prevScan = obj.m_currScan;
+            obj.m_currScan = zeros(obj.m_resolution);
+            
+            for i = 1:length(range) % Convert the Lidar Scan to down graded point map
+                if (range(i) < obj.m_maxRange)
+                    nodeX = (range(i) * cos(angle(i)))/obj.m_maxRange * (obj.m_resolution/2) + obj.m_resolution/2;
+                    nodeY = (range(i) * sin(angle(i)))/obj.m_maxRange * (obj.m_resolution/2) + obj.m_resolution/2;
+                    obj.m_currScan(round(nodeY), round(nodeX)) = 1;
+                end
+            end
+            if not (obj.m_isFirst) 
+                % init the map with start point
+                obj.m_currPos(1) = 0;
+                obj.m_currPos(2) = 0;
+                obj.m_isFirst = true;
+            else
+                % Iterate the new result with the previous scan to check the moveing distance
+                obj.m_currPos = getCurrPosition(obj.m_currPos, obj.m_currScan, obj.m_prevScan, obj.m_resolution, obj.m_maxSearch);
+            end
+            % After the localization result calculated, Update the map
+            for i = 1:obj.m_resolution
+                for j = 1:obj.m_resolution
+                    mapX = i - obj.m_resolution/2 + obj.m_currPos(1) + obj.m_MapCenter(1);
+                    mapY = j - obj.m_resolution/2 + obj.m_currPos(2) + obj.m_MapCenter(2);
+                    currVal = obj.m_Map(mapY, mapX);
+                    obj.m_Map(mapY, mapX) = currVal + obj.m_currScan(j,i);
+                end
+            end
+        end
+
+        function cuurPos = GetCurrLoc(obj)
+            cuurPos = obj.m_currPos;
+        end
+
+        function map = GetMap(obj)
+            map = obj.m_Map;
+        end
+    end
+end
---
+++ slamSimAlg2.m
@@ -0,0 +1,67 @@
+load slamRobotTrajectory.mat 
+
+w = vrworld('slamSimulatedWorld.x3d');
+open(w);
+
+vrf = vrfigure(w);
+
+robotVRNode = vrnode(w,'Robot');
+robotVRNode.children.translation = [trajectory(1,1) 0 trajectory(1,2)];
+robotVRNode.children.rotation = [0 1 0 0];
+
+lidarVRNode = vrnode(w,'LIDAR_Sensor');
+angles  = 180:-1.5:-178.5;
+angles = deg2rad(angles)';
+
+pause(1);
+
+maxLidarRange = 8;
+mapResolution = 1000;
+%mapResolution = 80;
+mapSearchRange = mapResolution / 20;
+mapSize = mapResolution * 2;
+robotSlamObj = slamObjAlg2(maxLidarRange, mapResolution, mapSearchRange, mapSize);
+controlRate = rateControl(10);
+
+posArray = zeros(200, 2);
+
+% Create Figures
+mapFig = figure();
+localizationFig = figure();
+figure(localizationFig), title('Localization Result');
+
+for i=1:length(trajectory)
+    % Use translation property to move the robot. 
+    robotVRNode.children.translation = [trajectory(i,1) 0 trajectory(i,2)];
+    vrdrawnow;
+    
+    % Read the range readings obtained from lidar sensor of the robot.
+    range = lidarVRNode.pickedRange;
+    
+    % The simulated lidar readings will give -1 values if the objects are
+    % out of range. Make all these value to the greater than
+    % maxLidarRange.
+    range(range==-1) = maxLidarRange+2;
+
+    % Add new scan to SLAM Object
+    robotSlamObj = addNode(robotSlamObj, range, angles);
+
+    disp("Curr Position is ");
+    disp(GetCurrLoc(robotSlamObj));
+    posArray(i, :) = GetCurrLoc(robotSlamObj);
+    
+    hold on;
+    figure(mapFig), imagesc(GetMap(robotSlamObj));
+    set(gca,'YDir','normal') 
+    title('Mapping Result');
+    figure(localizationFig), plot(posArray(:,1), posArray(:,2), 'or')
+    figure(localizationFig), xlim([min(posArray(:,1))-mapSearchRange max(posArray(:,1))+mapSearchRange])
+    figure(localizationFig), ylim([min(posArray(:,2))-mapSearchRange max(posArray(:,2))+mapSearchRange])
+    hold off;
+
+    % waitfor(controlRate);
+end
+
+close(vrf);
+close(w);
+pause(1);
