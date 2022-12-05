b2d8be830fd71de405fc6869cc9881ee19a7f399
RexCai <crh0126@gmail.com>
2022-12-04 17:01:46 -0800

    Add Display of real path

--- slamSimAlg1.m
+++ slamSimAlg1.m
@@ -16,7 +16,7 @@ angles = deg2rad(angles)';
 pause(1);
 
 maxLidarRange = 8;
-mapResolution = 200;
+mapResolution = 100;
 %mapResolution = 80;
 mapSearchRange = mapResolution / 10;
 mapSize = mapResolution * 2;
@@ -27,9 +27,9 @@ controlRate = rateControl(10);
 posArray = zeros(length(trajectory), 2);
 
 % Create Figures
-mapFig = figure();
 localizationFig = figure();
 figure(localizationFig), title('Localization Result');
+
 tic;
 for i=1:length(trajectory)
     % Use translation property to move the robot. 
@@ -52,20 +52,35 @@ for i=1:length(trajectory)
     posArray(i, :) = GetCurrLoc(robotSlamObj);
     
     hold on;
-    figure(mapFig), imagesc(GetMap(robotSlamObj));
-    set(gca,'YDir','normal') 
-    title('Mapping Result');
     displayVec = posArray;
     displayVec(i+1:end,:) = [];
-    figure(localizationFig), plot(displayVec(:,1), displayVec(:,2), 'or')
+    figure(localizationFig), plot(displayVec(:,1), displayVec(:,2), 'ob')
     figure(localizationFig), xlim([min(displayVec(:,1))-0.5 max(displayVec(:,1))+0.5])
     figure(localizationFig), ylim([min(displayVec(:,2))-0.5 max(displayVec(:,2))+0.5])
+    set(gca,'Ydir','reverse')
+    title('Localization Result');
     hold off;
 
     % waitfor(controlRate);
 end
 
 runTime = toc;
+
+mapFig = figure();
+
+hold on;
+figure(mapFig), imagesc(GetMap(robotSlamObj));
+set(gca,'YDir','normal') 
+title('Mapping Result');
+displayVec = posArray;
+displayVec(i+1:end,:) = [];
+figure(localizationFig), plot(displayVec(:,1), displayVec(:,2), 'ob', trajectory(:,1), trajectory(:,2), 'or')
+sizeVector = [displayVec;trajectory];
+figure(localizationFig), xlim([min(sizeVector(:,1))-0.5 max(sizeVector(:,1))+0.5])
+figure(localizationFig), ylim([min(sizeVector(:,2))-0.5 max(sizeVector(:,2))+0.5])
+set(gca,'Ydir','reverse')
+title('Localization Result');
+hold off;
 
 disp("Run Analysis: ");
 outDisp = sprintf('Total Run Time: %fs\n',runTime);
@@ -75,8 +90,8 @@ fprintf(outDisp);
 tError = posArray - trajectory;
 dErrorX = mean(tError(:,1));
 dErrorY = mean(tError(:,2));
-disp("Detla Error: ");
-outDisp = sprintf('X: %f, Y: %s, Sum %f\n', dErrorX, dErrorY, (dErrorX+dErrorY));
+disp("Mean Error: ");
+outDisp = sprintf('X: %f, Y: %s, Avg %f\n', dErrorX, dErrorY, (dErrorX+dErrorY));
 fprintf(outDisp);
 
 close(vrf);
--- slamSimAlg2.m
+++ slamSimAlg2.m
@@ -27,12 +27,10 @@ controlRate = rateControl(10);
 posArray = zeros(length(trajectory), 2);
 
 % Create Figures
-mapFig = figure();
 localizationFig = figure();
 figure(localizationFig), title('Localization Result');
 
 tic;
-
 for i=1:length(trajectory)
     % Use translation property to move the robot. 
     robotVRNode.children.translation = [trajectory(i,1) 0 trajectory(i,2)];
@@ -54,20 +52,35 @@ for i=1:length(trajectory)
     posArray(i, :) = GetCurrLoc(robotSlamObj);
     
     hold on;
-    figure(mapFig), imagesc(GetMap(robotSlamObj));
-    set(gca,'YDir','normal') 
-    title('Mapping Result');
     displayVec = posArray;
     displayVec(i+1:end,:) = [];
-    figure(localizationFig), plot(displayVec(:,1), displayVec(:,2), 'or')
+    figure(localizationFig), plot(displayVec(:,1), displayVec(:,2), 'ob')
     figure(localizationFig), xlim([min(displayVec(:,1))-0.5 max(displayVec(:,1))+0.5])
     figure(localizationFig), ylim([min(displayVec(:,2))-0.5 max(displayVec(:,2))+0.5])
+    set(gca,'Ydir','reverse')
+    title('Localization Result');
     hold off;
 
     % waitfor(controlRate);
 end
 
 runTime = toc;
+
+mapFig = figure();
+
+hold on;
+figure(mapFig), imagesc(GetMap(robotSlamObj));
+set(gca,'YDir','normal') 
+title('Mapping Result');
+displayVec = posArray;
+displayVec(i+1:end,:) = [];
+figure(localizationFig), plot(displayVec(:,1), displayVec(:,2), 'ob', trajectory(:,1), trajectory(:,2), 'or')
+sizeVector = [displayVec;trajectory];
+figure(localizationFig), xlim([min(sizeVector(:,1))-0.5 max(sizeVector(:,1))+0.5])
+figure(localizationFig), ylim([min(sizeVector(:,2))-0.5 max(sizeVector(:,2))+0.5])
+set(gca,'Ydir','reverse')
+title('Localization Result');
+hold off;
 
 disp("Run Analysis: ");
 outDisp = sprintf('Total Run Time: %fs\n',runTime);
@@ -77,8 +90,8 @@ fprintf(outDisp);
 tError = posArray - trajectory;
 dErrorX = mean(tError(:,1));
 dErrorY = mean(tError(:,2));
-disp("Detla Error: ");
-outDisp = sprintf('X: %f, Y: %s, Sum %f\n', dErrorX, dErrorY, (dErrorX+dErrorY));
+disp("Mean Error: ");
+outDisp = sprintf('X: %f, Y: %s, Avg %f\n', dErrorX, dErrorY, (dErrorX+dErrorY));
 fprintf(outDisp);
 
 close(vrf);
